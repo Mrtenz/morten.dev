ba910263a9f5f409b8b05d9c2fc1103a561f09db
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2020-06-03 11:06:13 +0200

    Remove framer-motion to reduce bundle size

--- src/components/Hamburger/Hamburger.tsx
+++ src/components/Hamburger/Hamburger.tsx
@@ -1,4 +1,3 @@
-import { motion } from 'framer-motion';
 import React, { FunctionComponent } from 'react';
 import styled from 'styled-components';
 import Line from './Line';
@@ -9,57 +8,22 @@ interface Props {
   onClick(): void;
 }
 
-const HamburgerSvg = styled(motion.svg)`
+const HamburgerSvg = styled.svg`
   cursor: pointer;
 `;
 
-const TOP_VARIANT = {
-  closed: {
-    rotate: 0,
-    translateY: 0
-  },
-  open: {
-    rotate: 45,
-    translateY: 2
-  }
-};
-
-const MIDDLE_VARIANT = {
-  closed: {
-    opacity: 1
-  },
-  open: {
-    opacity: 0
-  }
-};
-
-const BOTTOM_VARIANT = {
-  closed: {
-    rotate: 0,
-    translateY: 0
-  },
-  open: {
-    rotate: -45,
-    translateY: -2
-  }
-};
-
-const Hamburger: FunctionComponent<Props> = ({ isOpen, onClick }) => {
-  const variant = isOpen ? 'open' : 'closed';
-
-  return (
-    <HamburgerSvg
-      width="40"
-      height="25"
-      viewBox="0 0 6.4 4"
-      overflow="visible"
-      preserveAspectRatio="none"
-      onClick={onClick}>
-      <Line animate={variant} variants={TOP_VARIANT} x1="0" x2="6.4" y1="0" y2="0" />
-      <Line animate={variant} variants={MIDDLE_VARIANT} x1="0" x2="6.4" y1="2" y2="2" />
-      <Line animate={variant} variants={BOTTOM_VARIANT} x1="0" x2="6.4" y1="4" y2="4" />
-    </HamburgerSvg>
-  );
-};
+const Hamburger: FunctionComponent<Props> = ({ isOpen, onClick }) => (
+  <HamburgerSvg
+    width="40"
+    height="25"
+    viewBox="0 0 6.4 4"
+    overflow="visible"
+    preserveAspectRatio="none"
+    onClick={onClick}>
+    <Line variant="top" x1="0" x2="6.4" y1="0" y2="0" open={isOpen} />
+    <Line variant="middle" x1="0" x2="6.4" y1="2" y2="2" open={isOpen} />
+    <Line variant="bottom" x1="0" x2="6.4" y1="4" y2="4" open={isOpen} />
+  </HamburgerSvg>
+);
 
 export default Hamburger;
--- src/components/Hamburger/Line/Line.tsx
+++ src/components/Hamburger/Line/Line.tsx
@@ -1,16 +1,42 @@
-import { motion } from 'framer-motion';
-import styled from 'styled-components';
+import styled, { css, CSSObject } from 'styled-components';
 
-const Line = styled(motion.line).attrs({
+interface Props {
+  open: boolean;
+  variant: 'top' | 'middle' | 'bottom';
+}
+
+/*
+.attrs({
   vectorEffect: 'non-scaling-stroke',
   initial: 'closed',
   transition: {
     ease: 'easeOut',
     duration: 0.2
   }
-})`
+})
+ */
+
+const VARIANTS: { [key in 'top' | 'middle' | 'bottom']: CSSObject } = {
+  top: {
+    transform: 'rotate(45deg) translateY(0px)'
+  },
+  middle: {
+    opacity: 0
+  },
+  bottom: {
+    transform: 'rotate(-45deg) translateX(-3px) translateY(-0.8px)'
+  }
+};
+
+const Line = styled.line<Props>`
+  vector-effect: non-scaling-stroke;
   stroke: ${({ theme }) => theme.secondary};
   stroke-width: 0.5rem;
+  transform: rotate(0) translateY(0);
+  opacity: 1;
+  transition: 0.2s ease-out;
+
+  ${({ open, variant }) => open && css(VARIANTS[variant])}
 `;
 
 export default Line;
